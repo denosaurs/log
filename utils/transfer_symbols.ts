043d1f02340fc04a656c611d3d5d4074f139d268
Elias Sjögreen <elias@5monkeys.se>
2024-03-07 11:24:43 +0100

    fix: fmt and bump version

--- utils/transfer_symbols.ts
+++ utils/transfer_symbols.ts
@@ -2,17 +2,19 @@
  * Transfer symbols and their values as is from {@link value} to {@link target}.
  */
 export function transferSymbols<T>(value: T, target: T): T {
-  const properties = Object.getOwnPropertyNames(value) as (keyof typeof value)[];
+  const properties = Object.getOwnPropertyNames(
+    value,
+  ) as (keyof typeof value)[];
   const symbols = Object.getOwnPropertySymbols(value) as (keyof typeof value)[];
 
   for (const property of properties) {
     const entry = value[property];
 
-    if (typeof entry === 'symbol') {
+    if (typeof entry === "symbol") {
       target[property] = entry;
     }
 
-    if (typeof entry !== 'object') {
+    if (typeof entry !== "object") {
       continue;
     }
 
